c3ebf850f7e8256e538bf31d9926719c01a28020
Esley Gomes <edygomes04@hotmail.com>
2026-02-19 09:06:22 -0300

    exibe nome do usuário no painel

--- js/painel.js
+++ js/painel.js
@@ -6,6 +6,17 @@
             window.location.href = "index.html"
         }
 
+        //recupera o usuário salvo
+        const usuarioSalvo = JSON.parse(localStorage.getItem("usuario"));
+
+        //pega o span do HTML
+        const nomeSpan = document.getElementById("nomeUsuario");
+
+        //se existir usuário, mostra o nome
+        if (usuarioSalvo && nomeSpan) {
+            nomeSpan.innerText = usuarioSalvo.nome;
+        }
+
         //para sair
         function sair() {
             localStorage.removeItem("logado");
